e1e3caa34f69760c58aff5f01b3740fd12985e88
clemenceweiss1 <clemence.weiss1010@gmail.com>
2022-12-28 20:57:01 +0000

    pre-pocessing, add acronym + contraction expansion

--- code/Pre-Processing.py
+++ code/Pre-Processing.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import emoji
+import acronymDict as ad
+import re
+import contractions
 
 df = pd.read_csv(r'code\dataset.csv', encoding='utf-8')
 df.columns = ['ID', 'Date', 'Username', 'Text']
+
+#Remove rows with empty values
+df = df.dropna(axis=0)
 
 #replace emojis in text if the text value is not NaN
 def emoji_to_text(text):
@@ -18,8 +24,17 @@ print("Replaced emojis")
 df = df.apply(lambda x: x.replace({r'http\S+': ''}, regex=True))
 (print('Removed URLs'))
 
-#Remove punctuation
+#turn all text to lower case
+df['Text'] = df['Text'].str.lower()
+print('Turned text to lowercase')
 
+#expand contractions
+df['Text'] = df['Text'].str.replace("’", "'") #replace instances of different apostrophe so they are recognized by the contraction dictionary
+def expand_contractions(text):
+    return(re.sub(r"\b(\w+('\w+))\b", lambda x: contractions.CONTRACTIONS.get(x.group(1), x.group(1)), text)) #change regex from expanding acronyms to handle apostrophe
+df['Text'] = df['Text'].apply(expand_contractions)
+
+#Remove punctuation
 df['Text'] = df['Text'].str.replace(r'[^\w\s]+', ' ', regex=True)
 df['Text'] = df['Text'].str.replace('_', ' ')
 df['Text'] = df['Text'].str.replace(r'\n', '', regex=True)
@@ -35,7 +50,12 @@ print('Removed numbers')
 df['Text'] = df['Text'].str.replace(r'(\w)\1{2,}', r'\1', regex=True)
 print('Removed duplicate letters')
 
-#Expand Acronyms
+#Expand Acronyms using acronym dictionary
+def acronym_to_word(text):
+    return(re.sub(r"\b(\w+)\b", lambda x: ad.ACRONYM_TRANSLATE.get(x.group(1), x.group(1)), text))
+
+df['Text'] = df['Text'].apply(acronym_to_word)
+print('Replaced acronyms with their meaning')
 
 #Write the changes to csv file
 df.to_csv(r'code\dataset1.csv')
