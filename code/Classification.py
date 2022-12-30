2059d0be83150e8d9740b2c0ee9f6764dacb802d
clemenceweiss1 <clemence.weiss1010@gmail.com>
2022-12-30 21:31:14 +0000

    start with vader

--- code/Classification.py
+++ code/Classification.py
@@ -1,6 +1,8 @@
-import sklearn
+#Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text. Eighth International Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 df = pd.read_csv(r'code\dataset1.csv', encoding='utf-8')
 
@@ -8,3 +10,23 @@ vectoriser = TfidfVectorizer(stop_words = 'english', analyzer='word',ngram_range
 vectorised_values = vectoriser.fit_transform(df['Text'])
 
 print(vectorised_values)
+
+def sentence_score(sentence):
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    print("Overall sentiment dictionary is : ", sentiment_dict)
+    print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative")
+    print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral")
+    print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive")
+    print("Sentence Overall Rated As", end = " ")
+    # decide sentiment as positive, negative and neutral
+    if sentiment_dict['compound'] >= 0.05 :
+        print("Positive")
+    elif sentiment_dict['compound'] <= - 0.05 :
+        print("Negative")
+    else :
+        print("Neutral")
+
+sentence_score("I'm so happy today things are great and amazing hell yeah")
+
+print(SentimentIntensityAnalyzer.make_lex_dict())
