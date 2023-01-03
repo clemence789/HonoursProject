e11a636be50658b2d0ef9a8518f62642add0c449
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-03 13:33:50 +0000

    removed unused code

--- code/Algorithms.py
+++ code/Algorithms.py
@@ -14,13 +14,6 @@ ml_df = df.filter(['Score', 'Text'], axis=1)
 x = ml_df['Text']
 y = ml_df['Score']
 
-#y = list(y)
-#y = np.array(y)
-#y = [str(i) for i in y]
-#x = list(x)
-#x = np.array(x, dtype=str)
-#y = [str(i) for i in x]
-
 #split dataset into 80 for training and 20 for testing
 x_train, x_test, y_train, y_test = tts(x, y, test_size = 0.2, random_state = 0)
 
@@ -32,9 +25,6 @@ x_test_tfidf = vectoriser.transform(x_test)
 model = MultinomialNB()
 model.fit(x_train_tfidf, y_train)
 
-#x_train_tfidf = x_train_tfidf.reshape(-1,1)
-#x_test_tfidf = x_test_tfidf.reshape(-1,1)
-
 model.score(x_test_tfidf, y_test)
 
 y_pred = model.predict(x_test_tfidf)
@@ -42,5 +32,3 @@ y_pred = model.predict(x_test_tfidf)
 score1 = metrics.accuracy_score(y_test, y_pred)
 
 print(score1)
-
-#ml_df.to_csv(r'code\dataset2.csv')
