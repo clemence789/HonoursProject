0cf1ce569ecb4bd6f092b61e3912212a669e609d
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-03 15:31:04 +0000

    remove unnecessary line

--- code/GUI.py
+++ code/GUI.py
@@ -13,9 +13,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn import metrics
 
-
-#sg.Window(title = "Twitter Sentiment Classifier", layout=[[]], margins=(300, 200)).read()
-
 layout = [[sg.Text("Hello please enter your twitter bearer token")], [sg.Button("OK")]]
 sg.Window("Twitter Sentiment Classifier", layout)
 
