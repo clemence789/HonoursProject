c677d83e40010d47e2c7f21f2559127233de8d40
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-06 21:24:29 +0000

    call functions when data is passed

--- WebApp/webapp/tweetSentiment/views.py
+++ WebApp/webapp/tweetSentiment/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tweetSentiment.forms import DataEntryForm
 from tweetSentiment import preprocessing
+from .models import RequestedData
 
 def index(request):
     return HttpResponse("You're at the sentiment analysis index.")
@@ -16,9 +17,14 @@ def entry(request):
         if form.is_valid():
             bearer_token = form.cleaned_data['bearerToken']
             keywords = form.cleaned_data['keywords']
-            preprocessing.collectTweets(bearer_token, keywords)
-            preprocessing.cleanTweets()
-
+            username = form.cleaned_data['username']
+            if keywords is not '':
+                preprocessing.collectTweetsKeywords(bearer_token, keywords)
+                preprocessing.cleanTweets()
+            elif username is not '':
+                preprocessing.collectTweetsUsername(bearer_token, username)
+                preprocessing.cleanTweets()
+            
             return redirect('results')
 
     else:
