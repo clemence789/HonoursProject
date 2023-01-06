4591c0d8f66585d40b379278252fca2b2521d371
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-06 21:23:10 +0000

    registered model

--- WebApp/webapp/tweetSentiment/admin.py
+++ WebApp/webapp/tweetSentiment/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import BearerToken
+from .models import RequestedData
 
 # Register your models here.
-admin.site.register(BearerToken)
+admin.site.register(RequestedData)
