1818dd684f820e1110734513bde22f271e6e2b77
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-06 21:23:52 +0000

    forms fields

--- WebApp/webapp/tweetSentiment/forms.py
+++ WebApp/webapp/tweetSentiment/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class DataEntryForm(forms.Form):
     bearerToken = forms.CharField(label="Bearer Token")
-    keywords = forms.CharField(label = 'Keyword(s)')
+    keywords = forms.CharField(label = 'Keyword(s)', required=False)
+    username = forms.CharField(label = 'Username', required=False)
