cd1fcdddf6dc36044f7ed66d01a0359a2d7981c1
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-07 09:34:23 +0000

    changed database fields

--- WebApp/webapp/tweetSentiment/models.py
+++ WebApp/webapp/tweetSentiment/models.py
@@ -3,6 +3,5 @@ from django.db import models
 # Create your models here.
 class RequestedData(models.Model):
     tweet_text = models.CharField(max_length = 500)
-    tweet_username = models.CharField(max_length = 500)
     tweet_sentiment = models.CharField(max_length = 1)
 
