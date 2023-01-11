81de4e12e3a546a7c260f6a31778a15c1b988563
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-11 12:31:24 +0000

    add number of tweets to form

--- WebApp/webapp/tweetSentiment/forms.py
+++ WebApp/webapp/tweetSentiment/forms.py
@@ -2,5 +2,6 @@ from django import forms
 
 class DataEntryForm(forms.Form):
     bearerToken = forms.CharField(label="Bearer Token")
+    number_of_tweets = forms.IntegerField(label="Number of Tweets")
     keywords = forms.CharField(label = 'Keyword(s)', required=False)
     username = forms.CharField(label = 'Username', required=False)
