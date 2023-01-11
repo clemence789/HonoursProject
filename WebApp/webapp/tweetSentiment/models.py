95b9c29b8c2233aab98aa373ae20d8cd673f7fcf
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-11 12:30:38 +0000

    add intial tweet to database

--- WebApp/webapp/tweetSentiment/models.py
+++ WebApp/webapp/tweetSentiment/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class RequestedData(models.Model):
-    tweet_text = models.CharField(max_length = 500)
+    tweet_text = models.CharField(max_length = 5000)
+    tweet_text_clean = models.CharField(max_length = 5000, default="na")
     tweet_sentiment = models.CharField(max_length = 1)
     request_number = models.IntegerField()
 
