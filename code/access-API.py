803f34aa8d036a240e281439d211ede035897cd2
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-11 12:28:53 +0000

    change keywords for dataset

--- code/access-API.py
+++ code/access-API.py
@@ -21,7 +21,53 @@ max_results = 100
 iteration = 0
 while iteration < 4:
     tweets = client.search_recent_tweets(
-        "upset OR angry OR disgusted OR worst OR hate OR detest -is:retweet lang:en",
+        "angry OR disgusted OR worst OR hate OR detest -is:retweet lang:en",
+        max_results = max_results, #max results received, has to be number between 10 and 100
+        expansions=['author_id'], #expansions allows to get the username in 'user_fields'
+        tweet_fields = ['id','created_at','text', 'lang'],
+        user_fields = ['username']
+        )
+
+    data = []
+    time_format = "%A %B %d %H:%M:%S %Y" #Format the date and time should be in
+    for i in tweets.data:
+        in_tweet = []
+        in_tweet.append(str(i.id))
+        times = datetime.strftime(i.created_at, time_format)
+        in_tweet.append(str(times))
+        in_tweet.append(str(i.text))
+        data.append(in_tweet)
+    
+    len1 = len(tweets.data)
+    len2 = len(tweets.includes['users'])
+
+    #Equalize the number of tweets and users collected
+    if len1 != len2:
+        difference = len1 - len2
+        j = 0
+        while j < difference:
+            tweets.includes['users'].append('')
+            j+=1
+    
+    print('Tweets collected: ' + str(len(tweets.data)))
+
+    i = 0
+    while i < len(tweets.data):
+        data[i].insert(2, str(tweets.includes['users'][i]))
+        i+=1
+
+    #write to csv
+    with open(r'code\dataset.csv', 'a', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        #rows
+        writer.writerows(data)
+
+        iteration+=1
+
+iteration = 0
+while iteration < 4:
+    tweets = client.search_recent_tweets(
+        "happy OR joy OR love OR great OR amazing OR positive OR wonderful -is:retweet lang:en",
         max_results = max_results, #max results received, has to be number between 10 and 100
         expansions=['author_id'], #expansions allows to get the username in 'user_fields'
         tweet_fields = ['id','created_at','text', 'lang'],
