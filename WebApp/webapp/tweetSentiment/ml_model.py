7d677d9ce6c8ff229a15ef5b2380b0d927a5669f
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-11 12:31:43 +0000

    working tfidf vectorizer

--- WebApp/webapp/tweetSentiment/ml_model.py
+++ WebApp/webapp/tweetSentiment/ml_model.py
@@ -1,14 +1,12 @@
 import pickle
-from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer, TfidfVectorizer
 import numpy as np
 
 #apply ml model
 def prediction_model(tweet):
-    transformer = TfidfTransformer()
-    loaded_vec = CountVectorizer(decode_error = "replace", vocabulary=pickle.load(open(r'tweetSentiment/tfidf.sav', "rb")))
-    tweet = transformer.fit(loaded_vec.fit_transform(np.array([tweet]).flatten()))
+    loaded_vec = pickle.load(open(r'tweetSentiment/tfidf.sav', "rb"))
+    tweet = loaded_vec.transform(np.array([tweet]))
 
-    
     nb = pickle.load(open(r'tweetSentiment/finalized_model.sav', 'rb'))
     prediction = nb.predict(tweet)
     return prediction
