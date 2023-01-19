c3132e4a25c756fbd9c1fd517d8cff61a657d424
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-19 10:02:14 +0000

    add validators

--- WebApp/webapp/tweetSentiment/forms.py
+++ WebApp/webapp/tweetSentiment/forms.py
@@ -3,7 +3,8 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 class DataEntryForm(forms.Form):
-    #add fields in form
+    
+    #number of tweets input validator
     def validate_number(value):
         if value < 10:
             raise ValidationError(
@@ -15,7 +16,18 @@ class DataEntryForm(forms.Form):
                 _('%(value)s is too large'),
                 params={'value': value},
             )
+    
+    #username input field validator
+    def validate_username(value):
+        punctuation = """!"#$%&'()*+, -./:;<=>?@[\]^`{|}~"""
+        if any(char in punctuation for char in value) :
+            raise ValidationError(
+                _('Please enter a valid username'),
+            )
+
+    
+    #add fields in form
     bearerToken = forms.CharField(label="Bearer Token") #bearer token
     number_of_tweets = forms.IntegerField(label="Number of Tweets", validators=[validate_number]) #number of tweets
     keywords = forms.CharField(label = 'Keyword(s)', required=False) #keywords
-    username = forms.CharField(label = 'Username', required=False) #username
+    username = forms.CharField(label = 'Username', required=False, validators=[validate_username]) #username
