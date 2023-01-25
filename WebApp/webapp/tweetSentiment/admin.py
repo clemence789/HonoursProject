423ee2bb72bbe8c9da926fd58977409ec288e748
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-25 21:13:24 +0000

    register new model

--- WebApp/webapp/tweetSentiment/admin.py
+++ WebApp/webapp/tweetSentiment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RequestedData
+from .models import RequestedData, NegativeTweets
 
 # Register your models here.
 admin.site.register(RequestedData)
+admin.site.register(NegativeTweets)
