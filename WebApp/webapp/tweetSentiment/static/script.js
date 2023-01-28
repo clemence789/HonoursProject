159c28d3912908a6ce1af9982a7449106bb32dfd
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-01-28 13:20:05 +0000

    add function for second table

--- WebApp/webapp/tweetSentiment/static/script.js
+++ WebApp/webapp/tweetSentiment/static/script.js
@@ -14,3 +14,8 @@ function filter(){
   document.getElementById('table_neg').style.display = 'inline';
   document.getElementById('neg_button').style.display = 'none';
 }
+
+function filterPersonal(){
+  document.getElementById('table_neg_personal').style.display = 'inline';
+  document.getElementById('neg_button_personal').style.display = 'none';
+}
