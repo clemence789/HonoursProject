42ba5482b36f25a6e8761200874cd148f652ef78
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-02-04 09:04:12 +0000

    turn everything into unicode

--- code/Algorithms.py
+++ code/Algorithms.py
@@ -8,14 +8,14 @@ from sklearn import metrics
 import pickle
 
 #read dataframe
-df = pd.read_csv(r'C:\Users\cleme\Documents\1HonoursProject\Code\dataset1.csv', encoding='utf-8')
+df = pd.read_csv(r'code\dataset1.csv', encoding='utf-8')
 
 #get only the score and text columns that will be used for the classification
 ml_df = df.filter(['Score', 'Text'], axis=1)
 
 #declaring feature vector and target variable
-x = ml_df['Text']
-y = ml_df['Score']
+x = ml_df['Text'].values.astype('U')
+y = ml_df['Score'].values.astype('U')
 
 #split dataset into 80% for training and 20% for testing
 x_train, x_test, y_train, y_test = tts(x, y, test_size = 0.2, random_state = 0)
@@ -58,37 +58,29 @@ print(metrics.confusion_matrix(y_test, y_pred))
 print(score_nb)
 
 #repeat with Logistic regression
-lr = LogisticRegression()
-lr.fit(x_train_tfidf, y_train)
+#lr = LogisticRegression()
+#lr.fit(x_train_tfidf, y_train)
 
-lr.score(x_test_tfidf, y_test)
+#lr.score(x_test_tfidf, y_test)
 
-y_pred = lr.predict(x_test_tfidf)
+#y_pred = lr.predict(x_test_tfidf)
 
-score_lr = metrics.accuracy_score(y_test, y_pred)
+#score_lr = metrics.accuracy_score(y_test, y_pred)
 
-print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
-print(metrics.confusion_matrix(y_test, y_pred))
-print(score_lr)
+#print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
+#print(metrics.confusion_matrix(y_test, y_pred))
+#print(score_lr)
 
 #repeat with SVM
-svm = SVC(kernel = 'linear')
-svm.fit(x_train_tfidf, y_train)
+#svm = SVC(kernel = 'linear')
+#svm.fit(x_train_tfidf, y_train)
 
-svm.score(x_test_tfidf, y_test)
+#svm.score(x_test_tfidf, y_test)
 
-y_pred = svm.predict(x_test_tfidf)
+#y_pred = svm.predict(x_test_tfidf)
 
-score_svm = metrics.accuracy_score(y_test, y_pred)
+#score_svm = metrics.accuracy_score(y_test, y_pred)
 
-print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
-print(metrics.confusion_matrix(y_test, y_pred))
-print(score_svm)
-
-#test results on test sentences
-text = ["war hate maul death kill", "love joy happy yay love life", "i guess this was alright it was not too bad", "war hate maul death kill"]
-loaded_vec = pickle.load(open(r'tfidf.sav', "rb"))
-text = loaded_vec.transform(text)
-prediction = nb.predict(text)
-print(prediction)
-
+#print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
+#print(metrics.confusion_matrix(y_test, y_pred))
+#print(score_svm)
