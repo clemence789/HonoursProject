5717dfca1d3458fe1f94944864095e035648b3ea
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-02-04 09:03:47 +0000

    fix the access to use paginator

--- code/access-API.py
+++ code/access-API.py
@@ -13,28 +13,47 @@ client = tweepy.Client(bearer_token = keys['bearer_token'], wait_on_rate_limit =
 #max results per page, number between 10 and 100
 max_results = 100
 
-#initialise iteration to get the amount of tweets needed
-iteration = 0
+paginator = tweepy.Paginator(client.search_recent_tweets, 
+        query = "angry OR disgusted OR worst OR hate OR detest -is:retweet lang:en",
+        max_results = 100, 
+        tweet_fields = ['id','created_at','text', 'lang'], limit = 900)
 
-#collect negative tweets
-while iteration < 4:
-    #get tweets based on keywords, in english and without retweets
-    tweets = client.search_recent_tweets(
-        "angry OR disgusted OR worst OR hate OR detest -is:retweet lang:en",
-        max_results = max_results, 
-        expansions=['author_id'], #expansions allows to get the username in 'user_fields'
-        tweet_fields = ['id','created_at','text', 'lang'],
-        user_fields = ['username']
-        )
+for page in paginator:
 
     #initialise array that will store tweets
     data = []
 
     #Format the date and time should be in
     time_format = "%A %B %d %H:%M:%S %Y" 
-    
-    #add necessary information from tweets to in_tweet array
-    for i in tweets.data:
+
+    for i in page.data:
+        in_tweet = []
+        in_tweet.append(str(i.id))#tweet id
+        times = datetime.strftime(i.created_at, time_format)#format date and time
+        in_tweet.append(str(times))#date and time
+        in_tweet.append(str(i.text))#text tweet
+        data.append(in_tweet)#add to nested array
+
+    #write to csv
+    with open(r'code\dataset.csv', 'a', encoding='utf-8', newline='') as f:
+        writer = csv.writer(f)
+        #rows
+        writer.writerows(data)
+
+paginator = tweepy.Paginator(client.search_recent_tweets, 
+        query = "happy OR joy OR love OR great OR amazing OR positive OR wonderful -is:retweet lang:en",
+        max_results = 100, 
+        tweet_fields = ['id','created_at','text', 'lang'], limit = 900)
+
+for page in paginator:
+
+    #initialise array that will store tweets
+    data = []
+
+    #Format the date and time should be in
+    time_format = "%A %B %d %H:%M:%S %Y" 
+
+    for i in page.data:
         in_tweet = []
         in_tweet.append(str(i.id))#tweet id
         times = datetime.strftime(i.created_at, time_format)#format date and time
@@ -42,24 +61,7 @@ while iteration < 4:
         in_tweet.append(str(i.text))#text tweet
         data.append(in_tweet)#add to nested array
     
-    len1 = len(tweets.data)
-    len2 = len(tweets.includes['users'])
-
-    #Equalize the number of tweets and users collected
-    if len1 != len2:
-        difference = len1 - len2
-        j = 0
-        while j < difference:
-            tweets.includes['users'].append('')
-            j+=1
-    
-    print('Tweets collected: ' + str(len(tweets.data)))
-
-    i = 0
-    #add the usernames to array of tweets
-    while i < len(tweets.data):
-        data[i].insert(2, str(tweets.includes['users'][i]))
-        i+=1
+    print(data)
 
     #write to csv
     with open(r'code\dataset.csv', 'a', encoding='utf-8', newline='') as f:
@@ -67,51 +69,3 @@ while iteration < 4:
         #rows
         writer.writerows(data)
 
-        iteration+=1
-
-#repeat to collect positive tweets
-iteration = 0
-while iteration < 4:
-    tweets = client.search_recent_tweets(
-        "happy OR joy OR love OR great OR amazing OR positive OR wonderful -is:retweet lang:en",
-        max_results = max_results, #max results received, has to be number between 10 and 100
-        expansions=['author_id'], #expansions allows to get the username in 'user_fields'
-        tweet_fields = ['id','created_at','text', 'lang'],
-        user_fields = ['username']
-        )
-
-    data = []
-    time_format = "%A %B %d %H:%M:%S %Y" #Format the date and time should be in
-    for i in tweets.data:
-        in_tweet = []
-        in_tweet.append(str(i.id))
-        times = datetime.strftime(i.created_at, time_format)
-        in_tweet.append(str(times))
-        in_tweet.append(str(i.text))
-        data.append(in_tweet)
-    
-    len1 = len(tweets.data)
-    len2 = len(tweets.includes['users'])
-
-    #Equalize the number of tweets and users collected
-    if len1 != len2:
-        difference = len1 - len2
-        j = 0
-        while j < difference:
-            tweets.includes['users'].append('')
-            j+=1
-    
-    print('Tweets collected: ' + str(len(tweets.data)))
-
-    i = 0
-    while i < len(tweets.data):
-        data[i].insert(2, str(tweets.includes['users'][i]))
-        i+=1
-
-    #write to csv
-    with open(r'code\dataset.csv', 'a', encoding='utf-8', newline='') as f:
-        writer = csv.writer(f)
-        #rows
-        writer.writerows(data)
-
-        iteration+=1
