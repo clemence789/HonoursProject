1e2e9cbfd1872f54f66f830fd1b532caa92d1d1b
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-02-07 15:30:01 +0000

    change labels

--- WebApp/webapp/tweetSentiment/forms.py
+++ WebApp/webapp/tweetSentiment/forms.py
@@ -28,6 +28,6 @@ class DataEntryForm(forms.Form):
     
     #add fields in form
     bearerToken = forms.CharField(label="Bearer Token") #bearer token
-    number_of_tweets = forms.IntegerField(label="Number of Tweets", validators=[validate_number]) #number of tweets
+    number_of_tweets = forms.IntegerField(label="Please enter a number between 10 and 100", validators=[validate_number]) #number of tweets
     keywords = forms.CharField(label = 'Keyword(s)', required=False) #keywords
     username = forms.CharField(label = 'Username', required=False, validators=[validate_username]) #username
