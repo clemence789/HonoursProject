393e76454f8cbb165447129327a0efee38de2392
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-02-07 10:42:28 +0000

    add k fold cross validation

--- code/Algorithms.py
+++ code/Algorithms.py
@@ -4,8 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, C
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
 from sklearn import metrics
+import sklearn.model_selection as model_selection
 import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import make_scorer, f1_score
 
 #read dataframe
 df = pd.read_csv(r'code\dataset1.csv', encoding='utf-8')
@@ -14,20 +20,14 @@ df = pd.read_csv(r'code\dataset1.csv', encoding='utf-8')
 ml_df = df.filter(['Score', 'Text'], axis=1)
 
 #declaring feature vector and target variable
-x = ml_df['Text'].values.astype('U')
-y = ml_df['Score'].values.astype('U')
-
-#split dataset into 80% for training and 20% for testing
-x_train, x_test, y_train, y_test = tts(x, y, test_size = 0.2, random_state = 0)
+x = ml_df['Text'].values.astype('U') #feature vector
+y = ml_df['Score'].values.astype('U') #target variable
 
 #Use TFIDF to vectorize tweets
 vectoriser = TfidfVectorizer()
 
-#On the training set
-x_train_tfidf = vectoriser.fit_transform(x_train)
-
-#On the test set
-x_test_tfidf = vectoriser.transform(x_test)
+#Vectorise the tweets
+x_tfidf = vectoriser.fit_transform(x)
 
 #save the tfidf model
 file_name = 'tfidf.sav'
@@ -35,27 +35,48 @@ pickle.dump(vectoriser, open("tfidf.sav", "wb"))
 
 #Use multinomial Naïve Bayes to classify test set
 nb = MultinomialNB()
-nb.fit(x_train_tfidf, y_train)
+svm = SVC(kernel = 'linear')
+lr = LogisticRegression()
+
+f1 = cross_val_score(estimator = nb, X= x_tfidf, y=y, cv=10, scoring= 'f1_macro')
+recall = cross_val_score(estimator = nb, X= x_tfidf, y=y, cv=10, scoring= 'recall_macro')
+precision = cross_val_score(estimator = nb, X= x_tfidf, y=y, cv=10, scoring= 'precision_macro')
+
+print("recall: ", np.mean(recall))
+print("precision: ", np.mean(precision))
+print("average f1: ", np.mean(f1))
+
+
+
+#kf = model_selection.KFold(n_splits = 10, shuffle = True, random_state=42)
+
+#print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std(), scores.f1()))
+
+
+
+#nb.fit(x_train_tfidf, y_train)
+
 
 #save the nb model
-filename = 'finalized_model.sav'
-pickle.dump(nb, open(filename, 'wb'))
+#filename = 'finalized_model.sav'
+#pickle.dump(nb, open(filename, 'wb'))
 
 
-nb.score(x_test_tfidf, y_test)
+#nb.score(x_test_tfidf, y_test)
 
 #print predictions of test set
-y_pred = nb.predict(x_test_tfidf)
-print(y_pred)
+#y_pred = nb.predict(x_test_tfidf)
+#print(y_pred)
 
 
 
 #Find accuracy
-score_nb = metrics.accuracy_score(y_test, y_pred)
+#score_nb = metrics.accuracy_score(y_test, y_pred)
 
-print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
-print(metrics.confusion_matrix(y_test, y_pred))
-print(score_nb)
+#print(metrics.classification_report(y_test, y_pred, target_names = ['1', '2', '3', '4', '5']))
+#print(metrics.confusion_matrix(y_test, y_pred))
+#print(score_nb)
+
 
 #repeat with Logistic regression
 #lr = LogisticRegression()
