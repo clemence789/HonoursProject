11e933e0408d98ebe2a78923aaa69006fc35a5c4
clemenceweiss1 <clemence.weiss1010@gmail.com>
2023-02-14 16:31:42 +0000

    comment out unused code when testing

--- code/Classification.py
+++ code/Classification.py
@@ -2,43 +2,43 @@
 import pandas as pd
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-df = pd.read_csv(r'code\dataset1.csv', encoding='utf-8') #read the dataframe with pre-processed tweets
+df = pd.read_csv(r'code\dataset_clean_2.csv', encoding='utf-8') #read the dataframe with pre-processed tweets
 
 # modified code from https://www.geeksforgeeks.org/python-sentiment-analysis-using-vader/
 #add sentiment score using VADER lexicon
-def sentiment_scores(sentence):
-    sentiment = ""
-    sentence = str(sentence)
-    sentiment_analyser = SentimentIntensityAnalyzer()
-    sentiment_dict = sentiment_analyser.polarity_scores(sentence) #addscore to each tweet
+#def sentiment_scores(sentence):
+ #   sentiment = ""
+  #  sentence = str(sentence)
+   # sentiment_analyser = SentimentIntensityAnalyzer()
+    #sentiment_dict = sentiment_analyser.polarity_scores(sentence) #addscore to each tweet
     
     #give sentiment score based on lexicon score
-    if sentiment_dict['compound'] < -0.60:
-        sentiment = '1'
-    elif sentiment_dict['compound'] <= -0.20:
-        sentiment = '2'
-    elif sentiment_dict['compound'] <= 0.20:
-        sentiment = '3'
-    elif sentiment_dict['compound'] <= 0.60:
-        sentiment = '4'
-    else:
-        sentiment = '5'
-    return sentiment
+#    if sentiment_dict['compound'] < -0.60:
+ #       sentiment = '1'
+  #  elif sentiment_dict['compound'] <= -0.20:
+   #     sentiment = '2'
+    #elif sentiment_dict['compound'] <= 0.20:
+   #     sentiment = '3'
+#    elif sentiment_dict['compound'] <= 0.60:
+ #       sentiment = '4'
+  #  else:
+   #     sentiment = '5'
+    #return sentiment
 
 #ranked 1 to 5 with 1 being very negative and 5 being very positive
 
-df['Score'] = df['Text'].apply(sentiment_scores) #add score to score column
+#df['Score'] = df['Text'].apply(sentiment_scores) #add score to score column
 
 #Shift score column to the front
-df = df[['Score'] + [col for col in df.columns if col != 'Classification']]
+#df = df[['Score'] + [col for col in df.columns if col != 'Classification']]
 
 #delete extra column created by appending classification to the dataframe
-df = df.drop(df.columns[1], axis=1)
+#df = df.drop(df.columns[1], axis=1)
 
-print("Added classification to every tweet")
+#print("Added classification to every tweet")
 
 #Write the changes to csv file
-df.to_csv(r'code\dataset1.csv')
+#df.to_csv(r'code\dataset_clean.csv')
 
 
 
@@ -68,4 +68,4 @@ df = df.drop(df.columns[1], axis=1)
 print("Added classification to every tweet")
 
 #Write the changes to csv file
-df.to_csv(r'code\dataset2.csv')
+df.to_csv(r'code\dataset_clean_2cat.csv')
